ed54b6f6f5df18bfc22a87c007051eb13085c3ea
antoraro <anton.rozdorozhnuk@gmail.com>
2020-02-22 01:09:40 +0200

    Changed site name

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Anton Rozdorozhniuk CV',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         textTheme: Theme.of(context).textTheme.apply(fontFamily: 'Open Sans'),
